fd21da892db2ad5187951b6cca9ff68a6c3d2fe3
caio lucena <caio.lcolaco@gmail.com>
2022-09-20 09:13:39 -0300

    Conection to database and show data

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/CaioLuColaco/api-golang-rest/database"
 	"github.com/CaioLuColaco/api-golang-rest/models"
 	"github.com/CaioLuColaco/api-golang-rest/routes"
 )
@@ -12,7 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando servidor Rest com go")
 	routes.HandleRequest()
 }
